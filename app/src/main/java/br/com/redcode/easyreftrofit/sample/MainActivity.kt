3dcb64226913e562ee1988d79512c5db14608f5d
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-11-07 12:17:54 -0200

    Migrating to android x and upgrading logging-interceptor

--- app/src/main/java/br/com/redcode/easyreftrofit/sample/MainActivity.kt
+++ app/src/main/java/br/com/redcode/easyreftrofit/sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package br.com.redcode.easyreftrofit.sample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {

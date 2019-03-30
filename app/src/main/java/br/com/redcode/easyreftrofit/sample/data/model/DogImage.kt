1579f63db3038638f7d5431405dcca1874d97b95
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-03-30 20:17:39 -0300

    Payload and model do handle 'DogImage'

---
+++ app/src/main/java/br/com/redcode/easyreftrofit/sample/data/model/DogImage.kt
@@ -0,0 +1,10 @@
+package br.com.redcode.easyreftrofit.sample.data.model
+
+/*
+    CREATED BY @PEDROFSN
+*/
+
+data class DogImage(
+        val image: String,
+        val status: String
+)
---
+++ app/src/main/java/br/com/redcode/easyreftrofit/sample/data/payloads/ResponseGETDogImage.kt
@@ -0,0 +1,19 @@
+package br.com.redcode.easyreftrofit.sample.data.payloads
+
+import br.com.redcode.base.extensions.extract
+import br.com.redcode.base.interfaces.Payload
+import br.com.redcode.easyreftrofit.sample.data.model.DogImage
+
+/*
+    CREATED BY @PEDROFSN
+*/
+
+data class ResponseGETDogImage(
+        val message: String?, // https://images.dog.ceo/breeds/pyrenees/n02111500_1233.jpg
+        val status: String? // success
+) : Payload<DogImage> {
+    override fun toModel() = DogImage(
+            image = extract safe message,
+            status = extract safe status
+    )
+}
